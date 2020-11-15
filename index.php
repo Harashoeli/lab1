11f455e0212f80a0663ac1776dd4c27547c47df6
Celen Aleck <69942591+Harashoeli@users.noreply.github.com>
2020-11-16 04:00:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,189 @@
-<!DOCTYPe html>
-<html>
-<head>
-<title>Personal Introduction</title>
-</head>
-<body>
-<h1>Hello!</h1>
-<h2>My name is Aleck Miguel Celen but you can call me Aleck</h2>
-<h3>It is my pleasure meeting you</h3>
-<p> Just like you I am an introvert but I don't know if you are an introvert well lets just say we have pretty much in common</p>
-<body style="background-color:gray;">
-<p style="color:white;">
-<p style="background-color:lightgrey;">
-<h1 style="background-color:lightgry;">
-</body>
-</html>
+<!DOCTYPE HTML>
 
+<html>
+	<head>
+		<title>My Personal Website</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<nav>
+						<ul>
+							<li><a href="#intro">Welcome</a></li>
+							<li><a href="#one">Who am I</a></li>
+							<li><a href="#two">What I do</a></li>
+
+						</ul>
+					</nav>
+				</div>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<section id="intro" class="wrapper style1 fullscreen fade-up">
+						<div class="inner">
+							<h1>My Personal Website</h1>
+							<p>Welcome to my Personal Website my name is Aleck Miguel Celen</p>
+							<a class="image"><img src="images/pic03.jpg" alt="" data-position="25% 25%" /></a>
+							</ul>
+						</div>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper style2 spotlights">
+						<section>
+							<div class="content">
+								<div class="inner">
+									<h2>Who am I?</h2>
+									<p>I am your average weeb who loves anime a lot and play games a lot not after I loss my laptop due to the rain that suddenly entered our roof and not being able to do tasks for a while but now I got it back and hoping I can finish these courses, I am really addicted to games and when I say addicted I also love them a lot, games made me and built me up to be alive, more alive I mean because games made me open my eyes that these type of enterntainment is what I want to do and that is to play and play lots of games, well not just play games I also often spend my time watching lots of anime too and reading mangas when I get bored or tired of waiting for the new episode of the anime I am watching, don't worry I am pretty much approachable(I think so) due to my introvertness and social anxiety that's why I'm not quite comfortable meeting new people but I can manage and still do my tasks.</p>
+									
+								</div>
+							</div>
+						</section>
+						<section>
+							
+							<div class="content">
+								<div class="inner">
+									<h2>My Favorite Games</h2>
+									<p>One of my favorite game is Apex Legend, Apex Legends is a battle royale type of game wherein you drop out of the sky and loot some items or weapons to fight other players who also dropped in your place or in different kind of place it gives me a thrill that I am looking for when I am fighting other players like my blood is getting boiled by the thrill it gives specially when there are other squad who joined your recently fight and you get to fight them too. The other one is League of Legends or LoL I myself is a Dota player but I got tired of it or bored even in Dota 2 I grew tired of playing it and tried out LoL for a moment then there everything changed and I got addicted to it to.</p>
+								</div>
+							</div>
+						</section>
+						<section>
+							
+							<div class="content">
+								<div class="inner">
+									<h2>My Favorite Anime</h2>
+									<p>My Favorite anime most of all is Angel Beats which is from a visual novel brand by Key studios and the writer Jun Maeda it started airing in February 4, 2010 with its first episode, Angel Beats is a drama anime though at first episode or the other episodes you don't even notice that it is a drama anime because the anime itself introduces the characters and the events are funny enough for you to lose track that the anime itself has a genre drama it also has actions with good visuals that can make you excited on each battle scene that is also thrilling enough to make you jump on conclusions on what are they're fighting for and why are they're fighting for it, it is a good anime that only has 12 episodes that has a good story enough to give you feels also. </p>
+								</div>
+							</div>
+						</section>
+					</section>
+
+				<!-- Two -->
+					<section id="two" class="wrapper style3 fade-up">
+						<div class="inner">
+							<h2>What I do</h2>
+							<p>Here are some of my works</p>
+						</div>
+							<div class="features">
+								<section>
+									<h3>A1:My Favorite Application</h3>
+									<a class="image"><img src="images/Untitled.jpg" alt="" data-position="25% 25%" /></a>
+									<a class="image"><img src="images/Untitled2.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>A2: Persona</h3>
+									<a class="image"><img src="images/p1.jpg" alt="" data-position="25% 25%" /></a>
+									<a class="image"><img src="images/p2.jpg" alt="" data-position="25% 25%" /></a>
+									<a class="image"><img src="images/p3.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>A4: Invitation Design Activity</h3>
+									<a class="image"><img src="images/a4.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>A6: Color and Typhography</h3>
+									<a class="image"><img src="images/a6 1.jpg" alt="" data-position="25% 25%" /></a>
+									<a class="image"><img src="images/a6 2.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>G1: What is User Design?</h3>
+									<a class="image"><img src="images/g1.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>G2: What is Design Thinking?</h3>
+									<a class="image"><img src="images/g2.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>G3: What is Prototyping?</h3>
+									<a class="image"><img src="images/g3.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>L1- UX Design: 1 Overview</h3>
+									<a class="image"><img src="images/l1.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>L2- UX Design: 2 Analyzing User Data</h3>
+									<a class="image"><img src="images/r2.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>L3- UX Design: 3 Creating Personas</h3>
+									<a class="image"><img src="images/r3.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>L4- UX Design: 4 Ideation</h3>
+									<a class="image"><img src="images/l4.png" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>L5- UX Design: 5 Creating Scenarios and Storyboards</h3>
+									<a class="image"><img src="images/l5.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>L6- UX Design: 6 Paper Prototyping</h3>
+									<a class="image"><img src="images/l6.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>L7- UX Design: 7 Implementation Planning</h3>
+									<a class="image"><img src="images/l7.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>L8- UX Foundations: Prototyping</h3>
+									<a class="image"><img src="images/l8.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>L9- Bootstrap 4 Essential Training</h3>
+									<a class="image"><img src="images/l9.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>R1: My First Few Weeks of USERDES Class</h3>
+									<a class="image"><img src="images/r1.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>R2 Introduction to Design Thinking Process</h3>
+									<a class="image"><img src="images/r22.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>R3 Need Finding (Design Problem)</h3>
+									<a class="image"><img src="images/r33.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>R4 - Information Architecture</h3>
+									<a class="image"><img src="images/r4.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>R5- UX Design: 1 Overview</h3>
+									<a class="image"><img src="images/r5.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+								<section>
+									<h3>R6- Color Theory and Typography</h3>
+									<a class="image"><img src="images/r6.jpg" alt="" data-position="25% 25%" /></a>
+								</section>
+							</div>
+						</div>
+					</section>
+
+			
+
+		<!-- Footer -->
+			
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
